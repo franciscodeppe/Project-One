b3f338bbf8b683bac2e4c239668fdcf54bec59f6
francisco <francisco.deppe@gmail.com>
2017-06-10 13:04:08 -0700

    firebase added

---
+++ javascript.js
@@ -0,0 +1,11 @@
+var config = {
+  apiKey: "AIzaSyB57KQ1u_6kNnlgEGrVMCOeX7YbSxErUJw",
+  authDomain: "project-one-49553.firebaseapp.com",
+  databaseURL: "https://project-one-49553.firebaseio.com",
+  projectId: "project-one-49553",
+  storageBucket: "project-one-49553.appspot.com",
+  messagingSenderId: "633822167881"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database()
