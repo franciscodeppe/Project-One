bd8b3a1bb77790e8d53def313c720974ff065000
francisco <francisco.deppe@gmail.com>
2017-06-15 21:12:47 -0700

    psuedo code for api added

--- ui.js
+++ ui.js
@@ -29,3 +29,23 @@ $("#nySlider").on("slide", function(slideEvt) {
 });
 
 $("#nySliderVal").text($("#nyCurrentSliderValLabel").attr("data-slider-value"))
+
+point to "#main"div on index.html
+
+$(yes/no switch)on("change" function() {
+	var newsDiv = $("<div>")
+	newsDiv.addClass("col-lg-4")
+	for (# of news inputs) {
+		var newsRow = $("<div>").addClass("row")
+		var newsImage = img from news source
+		newsImage.addClass(col-lg-5)
+		var content = contect from news source
+		content.addClass(col-lg-7)
+		newsRow.append(newsImage)
+		newsRow.append(content)
+
+		newsDiv.append(newsRow)
+	}
+
+	$("#main").append(newsDiv)
+})
