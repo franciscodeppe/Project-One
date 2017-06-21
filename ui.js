a5d8615300861e9ab1b794b36374b87b2a9643da
francisco <francisco.deppe@gmail.com>
2017-06-20 21:26:02 -0700

    Francisco work

--- ui.js
+++ ui.js
@@ -1,114 +1,12 @@
-var open = false;
-$("#header").on("click", function() {
-    if (open === false) {
-        open = true;
-        openNav()
-    } else {
-        open = false;
-        closeNav()
-    }
-});
 
-function openNav() {
-    document.getElementById("mySidenav").style.width = "300px";
-}
+  var config = {
+    apiKey: "AIzaSyB57KQ1u_6kNnlgEGrVMCOeX7YbSxErUJw",
+    authDomain: "project-one-49553.firebaseapp.com",
+    databaseURL: "https://project-one-49553.firebaseio.com",
+    projectId: "project-one-49553",
+    storageBucket: "project-one-49553.appspot.com",
+    messagingSenderId: "633822167881"
+  };
+  firebase.initializeApp(config);
 
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-}
-
-
-
-$("#nytSlider").slider();
-$("#nytSlider").on("slide", function(slideEvt) {
-    $("#nytSliderVal").text(slideEvt.value)
-    num = slideEvt.value;
-});
-
-$("#nySliderVal").text($("#nyCurrentSliderValLabel").attr("data-slider-value"))
-
-$("#bbcSlider").slider();
-$("#bbcSlider").on("slide", function(slideEvt) {
-    $("#bbcSliderVal").text(slideEvt.value)
-    num = slideEvt.value;
-});
-
-$("#bbcSliderVal").text($("#bbcCurrentSliderValLabel").attr("data-slider-value"))
-
-$("#twSlider").slider();
-$("#twSlider").on("slide", function(slideEvt) {
-    $("#twSliderVal").text(slideEvt.value)
-    num = slideEvt.value;
-});
-
-$("#twSliderVal").text($("#twCurrentSliderValLabel").attr("data-slider-value"))
-
-
-
-
-
-// ----------------------------------------
-// point to "#main"div on index.html
-
-let stateArr = [false, false, false];
-
-let togFn = function(x, y, z) {
-    if (stateArr[y] === false) {
-        stateArr[y] = true;
-        let slVal = $('#' + z).val();
-        $('#main').append($('<div id="newsDiv' + y + '"class="panel panel-default col-lg-4 newsDiv">' + "nyt" ));
-        printNews(x, slVal, y);
-    } else {
-        stateArr[y] = false;
-        $('#newsDiv' + y).remove();
-
-    }
-    //console.log(stateArr[x]);
-};
-
-let slideChange = function(x, y, z) {
-    if (stateArr[y] === true) {
-        $('#contentDiv' + y).remove();
-        printNews(x, z, y);
-    } else {
-        return false;
-    }
-};
-
-let printNews = function(x, y, z) {
-    //console.log(x);
-    //console.log(y);
-    console.log('success');
-    let newsAPI = '469cf0be81ab487c8d6f31374930c8bd';
-    // let queryURL = 'https://api.nytimes.com/svc/topstories/v2/home.json?' + $.param({
-    //   'api-key': "e77c50dfeb48404d9461aad63e81fc72"});
-    let queryURL = 'https://newsapi.org/v1/articles?source=' + x + '&sortBy=top&apiKey=' + newsAPI;
-    $.ajax({
-        url: queryURL,
-        method: 'GET',
-    }).done(function(snapshot) {
-        console.log(snapshot);
-		var panelHeading = $('<div ' + 'class="panel-heading">' )
-		var contentDiv = $('<div id="contentDiv' + z + '"class="panel-body newsContent">');
-
-        for (let i = 0; i < y && i < snapshot.articles.length; i++) {
-
-            var newsRow = $('<div class="row content_row">');
-            var newsImage = $('<img src="' + snapshot.articles[i].urlToImage + '"class="row content_image">');
-            var content = $('<div class="row article_content">');
-            content.html('<p><a href="' + snapshot.articles[i].url + '">' + snapshot.articles[i].title + '</a>' +
-                '<p>' + snapshot.articles[i].description + '</p>');
-            newsRow.append(newsImage);
-            newsRow.append(content);
-            contentDiv.append(newsRow);
-			$('#newsDiv' + z).append(panelHeading);
-            $('#newsDiv' + z).append(contentDiv);
-        }
-
-    }).fail(function(err) {
-        throw err;
-    });
-
-};
+let database = firebase.database()
--- ui2.js
+++ ui2.js
@@ -4,31 +4,31 @@ $("#header").on("click", function() {
         open = true;
         openNav()
     } else {
-		open= false;
-		closeNav()
-	}
+        open = false;
+        closeNav()
+    }
 });
 
 function openNav() {
-    $("#mySidenav").css('width', '300px' )
-    $("#main").css('marginLeft', '300px' )
+    $("#mySidenav").css('width', '300px')
+    $("#main").css('marginLeft', '300px')
     $("#main").css('width', '100%')
-	$(".newsDiv").css('width','40%')
+    $(".newsDiv").css('width', '40%')
 }
 
 
 function closeNav() {
-    $("#mySidenav").css('width', '0' )
-    $("#main").css('marginLeft', '20px' )
+    $("#mySidenav").css('width', '0')
+    $("#main").css('marginLeft', '20px')
     $("#main").css('width', '100%')
-	$(".newsDiv").css('width','40%')
+    $(".newsDiv").css('width', '40%')
 }
 
 // sliders-------------------------------
 $("#nytSlider").slider();
 $("#nytSlider").on("slide", function(slideEvt) {
     $("#nytSliderVal").text(slideEvt.value)
-	num = slideEvt.value;
+    num = slideEvt.value;
 });
 
 $("#nySliderVal").text($("#nyCurrentSliderValLabel").attr("data-slider-value"))
@@ -36,7 +36,7 @@ $("#nySliderVal").text($("#nyCurrentSliderValLabel").attr("data-slider-value"))
 $("#bbcSlider").slider();
 $("#bbcSlider").on("slide", function(slideEvt) {
     $("#bbcSliderVal").text(slideEvt.value)
-	num = slideEvt.value;
+    num = slideEvt.value;
 });
 
 $("#bbcSliderVal").text($("#bbcCurrentSliderValLabel").attr("data-slider-value"))
@@ -44,7 +44,7 @@ $("#bbcSliderVal").text($("#bbcCurrentSliderValLabel").attr("data-slider-value")
 $("#fortuneSlider").slider();
 $("#fortuneSlider").on("slide", function(slideEvt) {
     $("#fortuneSliderVal").text(slideEvt.value)
-	num = slideEvt.value;
+    num = slideEvt.value;
 });
 
 $("#fortuneSliderVal").text($("#fortuneCurrentSliderValLabel").attr("data-slider-value"))
@@ -52,7 +52,7 @@ $("#fortuneSliderVal").text($("#fortuneCurrentSliderValLabel").attr("data-slider
 $("#timeSlider").slider();
 $("#timeSlider").on("slide", function(slideEvt) {
     $("#timeSliderVal").text(slideEvt.value)
-	num = slideEvt.value;
+    num = slideEvt.value;
 });
 
 $("#timeSliderVal").text($("#timeCurrentSliderValLabel").attr("data-slider-value"))
@@ -60,7 +60,7 @@ $("#timeSliderVal").text($("#timeCurrentSliderValLabel").attr("data-slider-value
 $("#wsjSlider").slider();
 $("#wsjSlider").on("slide", function(slideEvt) {
     $("#wsjSliderVal").text(slideEvt.value)
-	num = slideEvt.value;
+    num = slideEvt.value;
 });
 
 $("#nflSliderVal").text($("#nflCurrentSliderValLabel").attr("data-slider-value"))
@@ -68,7 +68,7 @@ $("#nflSliderVal").text($("#nflCurrentSliderValLabel").attr("data-slider-value")
 $("#nflSlider").slider();
 $("#nflSlider").on("slide", function(slideEvt) {
     $("#nflSliderVal").text(slideEvt.value)
-	num = slideEvt.value;
+    num = slideEvt.value;
 });
 
 $("#nflSliderVal").text($("#nflCurrentSliderValLabel").attr("data-slider-value"))
@@ -76,7 +76,7 @@ $("#nflSliderVal").text($("#nflCurrentSliderValLabel").attr("data-slider-value")
 $("#mtvSlider").slider();
 $("#mtvSlider").on("slide", function(slideEvt) {
     $("#mtvSliderVal").text(slideEvt.value)
-	num = slideEvt.value;
+    num = slideEvt.value;
 });
 
 $("#cnnSliderVal").text($("#cnnCurrentSliderValLabel").attr("data-slider-value"))
@@ -84,98 +84,116 @@ $("#cnnSliderVal").text($("#cnnCurrentSliderValLabel").attr("data-slider-value")
 $("#cnnSlider").slider();
 $("#cnnSlider").on("slide", function(slideEvt) {
     $("#cnnSliderVal").text(slideEvt.value)
-	num = slideEvt.value;
+    num = slideEvt.value;
 });
 
 $("#cnnSliderVal").text($("#cnnCurrentSliderValLabel").attr("data-slider-value"))
 
 
+// firebase
 
+var config = {
+    apiKey: "AIzaSyB57KQ1u_6kNnlgEGrVMCOeX7YbSxErUJw",
+    authDomain: "project-one-49553.firebaseapp.com",
+    databaseURL: "https://project-one-49553.firebaseio.com",
+    projectId: "project-one-49553",
+    storageBucket: "project-one-49553.appspot.com",
+    messagingSenderId: "633822167881"
+};
+firebase.initializeApp(config);
 
+let database = firebase.database()
 
+database.ref().on('value',function(snapshot) {
+	stateArr = snapshot.stateArr
+})
+)
 
 
 // ----------------------------------------
 // point to "#main"div on index.html
 
-let stateArr = [false, false, false, false, false, false, false, false, false, false,];
+let stateArr = [false, false, false, false, false, false, false, false, false, false, ];
 let titleArr = ['NYT', 'BBC', 'Fortune', 'Time', 'WSJ', 'NFL', 'MTV', 'CNN'];
 
-let togFn = function (x, y, z) { //x= url string, y=index value, z=slider id
-  if (stateArr[y] === false) {
-    stateArr[y] = true;
-    let slVal = $('#' + z).val();
-    printNews(x, y, slVal);
-  } else {
-    stateArr[y] = false;
-    $('#newsDiv' + y).remove();
-  }
+let togFn = function(x, y, z) { //x= url string, y=index value, z=slider id
+    if (stateArr[y] === false) {
+        stateArr[y] = true;
+        let slVal = $('#' + z).val();
+        printNews(x, y, slVal);
+    } else {
+        stateArr[y] = false;
+        $('#newsDiv' + y).remove();
+    }
 };
 
-let slideChange = function (x, y, z) { //x=url string, y=index value, z= slider value
-  if (stateArr[y] === true) {
-    printNews(x, y, z);
-  } else {
-    return false;
-  }
+let slideChange = function(x, y, z) { //x=url string, y=index value, z= slider value
+    if (stateArr[y] === true) {
+        printNews(x, y, z);
+    } else {
+        return false;
+    }
 };
 
-let printNews = function (x, y, z) {//x=URL string, y=index value , z =slider val
-  let newsAPI = '469cf0be81ab487c8d6f31374930c8bd';
-  let queryURL = 'https://newsapi.org/v1/articles?source=' + x + '&sortBy=top&apiKey=' + newsAPI;
-  $.ajax({
-    url: queryURL,
-    method: 'GET',
-  }).done(function(snapshot) {
+let printNews = function(x, y, z) { //x=URL string, y=index value , z =slider val
+    let newsAPI = '469cf0be81ab487c8d6f31374930c8bd';
+    let queryURL = 'https://newsapi.org/v1/articles?source=' + x + '&sortBy=top&apiKey=' + newsAPI;
+    $.ajax({
+        url: queryURL,
+        method: 'GET',
+    }).done(function(snapshot) {
 
-    if ($('#newsDiv' + y).length === 0) {
-      $('#main').append('<div id="newsDiv'  + y + '"></div>')
-      $('#newsDiv' + y).addClass('row newsDiv carousel slide');
+        if ($('#newsDiv' + y).length === 0) {
+            $('#main').append('<div id="newsDiv' + y + '"></div>')
+            $('#newsDiv' + y).addClass('row newsDiv carousel slide');
 
-    } else {
-      $('#newsDiv' + y).empty();
-      console.log('success');
-    }
-    $('#newsDiv' + y).append('<h2 class="newsTitle">' + titleArr[y] + '</h2>');//DO NOT TOUCH
-    $('#newsDiv' + y).append(
-    '<a class="left carousel-control" href="#newsDiv'+ y +'" data-slide="prev">' +
-    '<span class="glyphicon glyphicon-chevron-left"></span>' +
-    '<span class="sr-only">Previous</span></a>' +
+        } else {
+            $('#newsDiv' + y).empty();
+            console.log('success');
+        }
+        $('#newsDiv' + y).append('<h2 class="newsTitle">' + titleArr[y] + '</h2>'); //DO NOT TOUCH
+        $('#newsDiv' + y).append(
+            '<a class="left carousel-control" href="#newsDiv' + y + '" data-slide="prev">' +
+            '<span class="glyphicon glyphicon-chevron-left"></span>' +
+            '<span class="sr-only">Previous</span></a>' +
 
-  '<a class="right carousel-control" href="#newsDiv'+ y +'" data-slide="next">' +
-    '<span class="glyphicon glyphicon-chevron-right"></span>' +
-    '<span class="sr-only">Next</span></a>'
-  )
-  $('#newsDiv' + y).append('<div id="carHolder'+ y +'" class="carousel-inner">')
+            '<a class="right carousel-control" href="#newsDiv' + y + '" data-slide="next">' +
+            '<span class="glyphicon glyphicon-chevron-right"></span>' +
+            '<span class="sr-only">Next</span></a>'
+        )
+        $('#newsDiv' + y).append('<div id="carHolder' + y + '" class="carousel-inner">')
 
-    for (let i = 0; i < z && i < snapshot.articles.length; i++) {
-      console.log(i);
-      let articale;
-      if (i === 0) {
-      article = $('<div class="container newsContent item active list-group-item">');
-    } else {
-      article = $('<div class="container newsContent item list-group-item">');
-    }
+        for (let i = 0; i < z && i < snapshot.articles.length; i++) {
+            console.log(i);
+            let articale;
+            if (i === 0) {
+                article = $('<div class="container newsContent item active list-group-item">');
+            } else {
+                article = $('<div class="container newsContent item list-group-item">');
+            }
 
-      let image = $('<img src="' + snapshot.articles[i].urlToImage +
-      '" class="img-responsive">');
+            let image = $('<img src="' + snapshot.articles[i].urlToImage +
+                '" class="img-responsive">');
 
-      let content = $('<div class="articleContent">');
+            let content = $('<div class="articleContent">');
 
-      content.html(
-        '<p><a href="' + snapshot.articles[i].url + '">' +
-        snapshot.articles[i].title + '</a></p>' +
-        '<p>' + snapshot.articles[i].description + '</p>'
-      );
-      article.append(image);
-      article.append(content);
-      $('#carHolder'+ y).append(article);
+            content.html(
+                '<p><a href="' + snapshot.articles[i].url + '">' +
+                snapshot.articles[i].title + '</a></p>' +
+                '<p>' + snapshot.articles[i].description + '</p>'
+            );
+            article.append(image);
+            article.append(content);
+            $('#carHolder' + y).append(article);
 
-    }
-    Sortable.create(main, { /*options */});
+            database.ref().set({
+                stateArr: stateArr
+            })
+        }
+        Sortable.create(main, { /*options */ });
 
-  }).fail(function(err) {
-    throw err;
-  });
+    }).fail(function(err) {
+        throw err;
+    });
 
 };
